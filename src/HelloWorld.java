89461112063d8756adb715bcc9f5ff6130f21792
Javier Razo <javier.razo1221@gmail.com>
2023-02-14 10:45:21 -0700

    added redo 1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,28 @@
 public class HelloWorld {
         public static void main(String[] args) {
             System.out.println("Hello, World!");
+
+            int myFavoriteNumber = 1221;
+            System.out.println(myFavoriteNumber);
+
+            String myString = "powder";
+            System.out.println(myString);
+
+            float myNumber = 123;
+            System.out.println(myNumber);
+//
+//            int x = 5;
+//            System.out.println(x++);
+//            System.out.println(x);
+
+            int x = 5;
+            System.out.println(++x);
+            System.out.println(x);
+
+
         }
     }
+
 
 
 
